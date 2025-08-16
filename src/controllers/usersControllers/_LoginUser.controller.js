c43670625ef9841525d08b62ad38c44b3e52e52d
Abubakar <abubakarzahid895@gmail.com>
2025-08-16 00:27:34 -0700

    files mised mach fiixed

---
+++ src/controllers/usersControllers/_LoginUser.controller.js
@@ -0,0 +1,38 @@
+import { User } from "../models/_User.model.js";
+import { custom_Error } from "../utils/_CustomErrorClass.js";
+import { requestError_Handler } from "../utils/_RequestWraper.js";
+import { genratingRefreshAndAccessTokens } from "../utils/_GeneratingRefreshAndAccessTokens.js"
+import { standardApi_Response } from "../utils/_ApiResponseClass.js";
+
+const loginUser = requestError_Handler(async (req, res, next) => {
+    const { email, password, username } = req.body
+    if (!email && !password) throw new custom_Error(400, "Email or password is requied")
+
+    const UserExisit = await User.findOne({ $and: [{ email }, { username }] })
+    if (!UserExisit) throw new custom_Error(400, "User doesnot exist")
+
+    const passwordCorrect = await UserExisit.isPasswordCorrect(password)
+
+    if (!passwordCorrect) throw new custom_Error(401, "Password is not correct")
+
+    const { AccessToken, RefreshToken } = await genratingRefreshAndAccessTokens(UserExisit._id)
+
+    const LoggedInUser = await User.findById(UserExisit._id).select("-password -refreshToken")
+
+    const Options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("AccessToken", AccessToken, Options)
+        .cookie("RefreshToken", RefreshToken, Options)
+        .json(
+            new standardApi_Response(
+                200, { LoggedInUser, AccessToken, RefreshToken }, "User Sucessfully LoggedIn"
+            )
+        )
+})
+
+export { loginUser }
---
+++ src/controllers/usersControllers/_RegisterUser.controller.js
@@ -0,0 +1,58 @@
+
+import { requestError_Handler } from "../utils/_RequestWraper.js"
+import { custom_Error } from "../utils/_CustomErrorClass.js"
+import { User } from "../models/_User.model.js"
+import { cloudinary_FileUplod } from "../utils/_Cloudinary.js"
+import { standardApi_Response } from "../utils/_ApiResponseClass.js"
+const registerUser = requestError_Handler(async (req, res, next) => {
+
+    const { email, fullname, password, userName } = req.body
+    const existedUser = await User.findOne({ $or: [{ email }, { userName }] })
+    let avatarLocalPath
+    let coverImageLocalPath
+
+    if (req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0) {
+        avatarLocalPath = req.files?.avatar[0]?.path;
+    }
+
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files?.coverImage[0]?.path;
+    }
+
+
+    const avatarCloud = await cloudinary_FileUplod(avatarLocalPath)
+    const coverImageCloud = await cloudinary_FileUplod(coverImageLocalPath)
+
+
+    if ([email, fullname, password].some(field => field == ""))
+        throw new custom_Error(404, "All fields are requied")
+
+    if (existedUser) throw new custom_Error(409, "Already existed")
+
+    if (!avatarLocalPath || !coverImageLocalPath) throw new custom_Error(400, "Avatar or Cover Image is requied")
+    if (!avatarCloud || !coverImageCloud) throw new custom_Error(400, "Avatar or Cover Image is not Uploded")
+   
+        const UserDatabase = await User.create({
+        fullname,
+        email,
+        password,
+        userName: userName.toLowerCase(),
+        avatar: avatarCloud?.url || "",
+        coverImage: coverImageCloud?.url || ""
+    })
+
+    const UserHasCreated = await User.findById(UserDatabase._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!UserHasCreated) throw new custom_Error(500, "User not created")
+
+    return res.status(201).json(
+        new standardApi_Response(200, "done", UserHasCreated)
+    )
+
+})
+
+
+
+export { registerUser }
